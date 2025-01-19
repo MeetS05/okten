ffd224fd08c9fc702b870e33323342d588f914a0
MeetS05 <meet.editsh@gmail.com>
2025-01-19 19:12:09 +0530

    19-01-2025

---
+++ asset/js/script.js
@@ -0,0 +1,17 @@
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+
+scrollTopBtn.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" 
+  });
+};
